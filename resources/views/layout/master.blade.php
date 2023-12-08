eb8c545f7738901dd002e9efdbbba03f9e1fa844
Hanis Zahira <haniszhr19@gmail.com>
2023-12-08 11:47:46 +0800

    feat : removing icon on page bar

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -9,11 +9,8 @@
   <meta content="" name="description">
   <meta content="" name="keywords">
 
-  <!-- Favicons -->
-  <!--<link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">-->
-
-
+  <link href="" rel="icon">
+  <link href="" rel="apple-touch-icon">
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
@@ -43,9 +40,6 @@
     <!-- Header part-->
     @include('layout.topbar')
     @yield('content')
-
-  <div id="preloader"></div>
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
   <script src="assets/vendor/aos/aos.js"></script>
