a9f4e902c2fdf1affa93016d45f5eed567708a9d
Hanis Zahira <haniszhr19@gmail.com>
2023-12-11 18:33:36 +0800

    feat : remove logo from tab bar

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -14,7 +14,7 @@
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
+  <link rel="icon" type="image/png" href="">
   <!--Envanto template -->
   <link rel="stylesheet" href="asset/css/bootstrap.min.css">
   <!-- Animate CSS -->
